ba8e71b2726b238e85dfd15500a67b66833c110e
Gustavo Dias A <gfdiasa@gmail.com>
2023-07-04 21:57:32 -0300

    Create mock data

---
+++ src/mocks/nativeBaseComponents.tsx
@@ -0,0 +1,18 @@
+const nativeBaseComponents = {
+  list: [
+    {
+      name: 'Button',
+    },
+    {
+      name: 'Input',
+    },
+    {
+      name: 'Radio',
+    },
+    {
+      name: 'Select',
+    },
+  ],
+};
+
+export default nativeBaseComponents;
